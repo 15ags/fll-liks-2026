950d18346bee73af824fb1cc13dd7c00bce767ee
15ags <15ags@proton.me>
2026-01-28 21:00:11 -0600

    added file for calibration

---
+++ reset.py
@@ -0,0 +1,11 @@
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Direction, Port
+from pybricks.pupdevices import Motor
+
+hub = PrimeHub()
+
+front_cat = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+back_cat = Motor(Port.E, Direction.CLOCKWISE)
+
+front_cat.run_target(100, 0)
+back_cat.run_target(100, 0)
