3c58d23ec7b4c7f848b84e097ffe0ac7f33cb267
15ags <15ags@proton.me>
2026-01-29 08:23:53 -0600

    added comments

--- reset.py
+++ reset.py
@@ -6,6 +6,6 @@ hub = PrimeHub()
 
 front_cat = Motor(Port.F, Direction.COUNTERCLOCKWISE)
 back_cat = Motor(Port.E, Direction.CLOCKWISE)
-# commit test
+# run this code to put the motors to 0, then put on the robot attachments and u are ready for running the robot code
 front_cat.run_target(200, 0)
 back_cat.run_target(200, 0)
